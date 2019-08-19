504938dc50fb48786cdc48d682fa88b69a70ba88
Vincent Sels <vincent_sels@hotmail.com>
2019-08-19 20:25:01 +0200

    Cleaned up functions a bit more

--- project/index.js
+++ project/index.js
@@ -10,22 +10,27 @@ const loadingSpan = document.getElementById('loading');
 const errorDialog = document.getElementById('error-dialog');
 
 const ALL_COUNTRIES_ENDPOINT = 'https://restcountries.eu/rest/v2/all';
+const SELECT_COUNTRY_TEXT = 'Select a country...';
 
 let countries = [];
 
+function addOptionToCountrySelectList(text) {
+  const option = document.createElement('option');
+  option.text = text;
+  option.value = text;
+  countriesSelectList.appendChild(option);
+}
+
 function populateSelectList() {
-  countries.forEach((country) => {
-    const option = document.createElement('option');
-    option.text = country.name;
-    option.value = country.name;
-    
-    countriesSelectList.appendChild(option);
-  });
+  addOptionToCountrySelectList(SELECT_COUNTRY_TEXT);
+
+  countries.forEach(country => addOptionToCountrySelectList(country.name));
+  
   loadCountriesButton.setAttribute('disabled', '');
   countriesSelectList.style.display = 'inline';
 }
 
-function showLoading(loading) {
+function showLoadingIndicator(loading) {
   loadingSpan.style.display = loading ? 'inline' : 'none';
 }
 
@@ -35,32 +40,34 @@ function showError(errorDetail) {
 }
 
 function getCountries() {
-  showLoading(true);
+  showLoadingIndicator(true);
   showError(null);
+
   fetch(ALL_COUNTRIES_ENDPOINT)
     .then((result) => result.json())
     .then((data) => {
       countries = data;
       populateSelectList();
-      showLoading(false);
     })
     .catch((error) => {
       showError(error);
-      showLoading(false);
-    });
+    })
+    .finally(() => showLoadingIndicator(false));
 }
 
-function selectCountry() {
-  const selectedCountryName = countriesSelectList.value;
-  const country = countries.find((c) => c.name === selectedCountryName);
-  
+function setCountryName(country) {
+  titleDiv.innerHTML = country.name;
+}
+
+function setCountryFlag(country) {
   flagDiv.innerHTML = '';
-  titleDiv.innerHTML = '';
   
   const img = document.createElement('img');
   img.src = country.flag;
   flagDiv.appendChild(img);
-  
+}
+
+function setCountryInfo(country) {
   // Translate the bordering countries' codes into their actual names
   const borderingCountries = country.borders.map(
     borderCode => countries.find(c => c.alpha3Code === borderCode).name).join(', ');
@@ -73,8 +80,21 @@ function selectCountry() {
     ? `Its capital is ${country.capital}.`
     : 'It has no capital.';
   
-  titleDiv.innerHTML = country.name;
-  infoDiv.innerHTML =  `${capitalText} ${borderingCountriesText}`;
+  infoDiv.innerHTML = `${capitalText} ${borderingCountriesText}`;
+}
+
+function selectCountry() {
+  if (countriesSelectList.value === SELECT_COUNTRY_TEXT) {
+    countryCardDiv.style.display = 'none';
+    return;
+  }
+
+  const selectedCountryName = countriesSelectList.value;
+  const country = countries.find((c) => c.name === selectedCountryName);
+  
+  setCountryFlag(country);
+  setCountryName(country);
+  setCountryInfo(country);
   
   countryCardDiv.style.display = 'block';
 }
